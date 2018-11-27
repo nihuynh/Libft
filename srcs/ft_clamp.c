90ff32594b72f6f544a548f4cfea29728e3bfef6
Nicolas Huynh <nico.huynh@gmail.com>
2018-11-27 03:06:02 +0100

    ft_clamp

--- srcs/ft_clamp.c
+++ srcs/ft_clamp.c
@@ -1,22 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ftmath.h                                           :+:      :+:    :+:   */
+/*   ft_clamp.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nihuynh <nihuynh@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/04/07 01:09:18 by nihuynh           #+#    #+#             */
-/*   Updated: 2018/11/27 03:02:52 by nihuynh          ###   ########.fr       */
+/*   Created: 2018/11/27 03:00:25 by nihuynh           #+#    #+#             */
+/*   Updated: 2018/11/27 03:05:03 by nihuynh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FTMATH_H
-# define FTMATH_H
+# include "ftmath.h"
 
-int		ft_abs(int value);
-int		ft_btw(int value, int a, int b);
-int		ft_min(int a, int b);
-int		ft_max(int a, int b);
-int		ft_clamp(int value, int low, int high);
-
-#endif
+ int     ft_clamp(int value, int low, int high)
+{
+	return (ft_min(ft_max(value, low), high)); 
+}
