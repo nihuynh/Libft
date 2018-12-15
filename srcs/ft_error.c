725459ed6c12705735440cfef78f9965901d9a34
nihuynh <nihuynh@student.42.fr>
2018-12-15 23:18:39 +0100

    comment on ft_error

--- srcs/ft_error.c
+++ srcs/ft_error.c
@@ -6,7 +6,7 @@
 /*   By: nihuynh <nihuynh@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/30 14:18:39 by nihuynh           #+#    #+#             */
-/*   Updated: 2018/12/15 22:04:16 by nihuynh          ###   ########.fr       */
+/*   Updated: 2018/12/15 23:18:13 by nihuynh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,13 @@
 #include <string.h>
 #include <stdlib.h>
 #include <sys/errno.h>
+
+/*
+** @ft_error
+** This will abort the program an give valuable intel on why and where
+** @param function (__func__)
+** @param line (__LINE__)
+*/
 
 void	ft_error(const char function[], int line)
 {
