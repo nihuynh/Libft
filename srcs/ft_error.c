fc3eb8c2aadffb6b390c0fb4d30aba124abd9fba
nihuynh <nihuynh@student.42.fr>
2018-12-15 23:15:43 +0100

    refactor ft_error

--- srcs/ft_error.c
+++ srcs/ft_error.c
@@ -6,7 +6,7 @@
 /*   By: nihuynh <nihuynh@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/30 14:18:39 by nihuynh           #+#    #+#             */
-/*   Updated: 2018/04/30 14:52:42 by nihuynh          ###   ########.fr       */
+/*   Updated: 2018/12/15 22:04:16 by nihuynh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,7 @@
 
 void	ft_error(const char function[], int line)
 {
-	ft_putstr("Error during ");
-	ft_putstr(function);
-	ft_print_value(" (at line : ", line);
-	ft_putstr(") => ");
-	ft_putendl_fd(strerror(errno), 2);
+	ft_printf("Error during %s (at line : %d) => %s\n",\
+			function, line, strerror(errno));
 	exit(errno);
 }
