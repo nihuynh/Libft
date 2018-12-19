8460aa73c14204b89779dee6262d8386b31ad13c
nihuynh <nihuynh@student.42.fr>
2018-12-19 22:05:53 +0100

    norme even the main

--- printf_main.c
+++ printf_main.c
@@ -6,33 +6,25 @@
 /*   By: nihuynh <nihuynh@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/08 21:23:49 by nihuynh           #+#    #+#             */
-/*   Updated: 2018/12/09 20:34:54 by nihuynh          ###   ########.fr       */
+/*   Updated: 2018/12/19 22:05:17 by nihuynh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "ftio.h"
-# include <stdio.h>
+#include "ftio.h"
+#include <stdio.h>
 
-void test1(void)
+void	test1(void)
 {
 	ft_printf("Just a string\n");
 	ft_printf("%s\n", "Just a string arg");
 	ft_printf("Enterlace %s\n", "string");
-}
-
-void test2(void)
-{
 	ft_printf("%d\n", 5000);
 	ft_printf("Enterlace %d\n", 5000);
-}
-
-void test3(void)
-{
 	ft_printf("%f\n", 3.14);
 	ft_printf("Enterlace %f\n", 3.14);
 }
 
-void test_float(void)
+void	test_float(void)
 {
 	float		f;
 	double		d;
@@ -45,20 +37,18 @@ void test_float(void)
 	ft_printf("ft = float %f double %lf ldouble %Lf\n", f, d, ld);
 }
 
-void test_pointer(void)
+void	test_pointer(void)
 {
 	int	pistache;
-	
+
 	pistache = 69;
-	printf("pointer %p\n", (void*) &pistache);
+	printf("pointer %p\n", (void*)&pistache);
 	ft_printf("pointer %p\n", &pistache);
 }
 
-int	main(void)
+int		main(void)
 {
 	test1();
-	test2();
-	test3();
 	test_float();
 	test_pointer();
 	return (0);
