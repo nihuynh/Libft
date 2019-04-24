8d9f63a68758e611c3f51016eedcd86597b436f5
Nicolas Huynh <nico.huynh@gmail.com>
2019-04-24 04:41:30 +0200

    adding stddef for linux compile

--- srcs/ft_curr_usec.c
+++ srcs/ft_curr_usec.c
@@ -6,11 +6,12 @@
 /*   By: nihuynh <nihuynh@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/18 11:28:34 by nihuynh           #+#    #+#             */
-/*   Updated: 2019/04/08 17:43:59 by nihuynh          ###   ########.fr       */
+/*   Updated: 2019/04/24 04:41:01 by nihuynh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <sys/time.h>
+#include <stddef.h>
 
 long	ft_curr_usec(void)
 {
