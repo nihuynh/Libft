d681d3a3c902f4f3dda23a2cc603a8648867cc71
Nicolas Huynh <nico.huynh@gmail.com>
2019-05-02 22:38:46 +0200

    void * are hard

--- srcs/list/ft_lstpushnew.c
+++ srcs/list/ft_lstpushnew.c
@@ -6,7 +6,7 @@
 /*   By: nihuynh <nihuynh@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/02 21:26:52 by nihuynh           #+#    #+#             */
-/*   Updated: 2019/05/02 21:35:55 by nihuynh          ###   ########.fr       */
+/*   Updated: 2019/05/02 22:36:40 by nihuynh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,13 @@ void	ft_lstpushnew(t_list **alst, const void *data, size_t content_size)
 {
 	t_list *node;
 
-	node = NULL;
 	if (data == NULL)
-		return ;
-	if (!(node = ft_lstnew(&data, content_size)))
-    	ft_error(__func__, __LINE__);
-	ft_lstadd(alst, node);
+		ft_error(__func__, __LINE__);
+	if ((node = ft_lstnew(data, content_size)))
+	{
+		node->next = *alst;
+		*alst = node;
+	}
+	else
+		ft_error(__func__, __LINE__);
 }
