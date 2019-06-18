af79545c9c8cdbc5690d6c57f303338272fc6071
Nicolas Huynh <nico.huynh@gmail.com>
2019-06-19 01:17:48 +0200

    ft_btwf added

--- srcs/math/ft_btwf.c
+++ srcs/math/ft_btwf.c
@@ -1,27 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ftmath.h                                           :+:      :+:    :+:   */
+/*   ft_btwf.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nihuynh <nihuynh@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/04/07 01:09:18 by nihuynh           #+#    #+#             */
-/*   Updated: 2019/06/19 01:14:59 by nihuynh          ###   ########.fr       */
+/*   Created: 2019/06/19 01:14:10 by nihuynh           #+#    #+#             */
+/*   Updated: 2019/06/19 01:15:30 by nihuynh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FTMATH_H
-# define FTMATH_H
+#include <math.h>
 
-int		ft_abs(int value);
-float	ft_absf(float value);
-int		ft_btw(int value, int a, int b);
-int		ft_min(int a, int b);
-float	ft_minf(float x, float y);
-int		ft_max(int a, int b);
-float	ft_maxf(float x, float y);
-int		ft_clamp(int value, int low, int high);
-float	ft_clampf(float value, float low, float high);
-float	ft_btwf(float value, float limit1, float limit2);
-
-#endif
+float	ft_btwf(float value, float limit1, float limit2)
+{
+	return (value >= fminf(limit1, limit2) && value <= fmaxf(limit1, limit2));
+}
